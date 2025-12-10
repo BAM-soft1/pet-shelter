616bd9cfbfb42b8dde5f8434942fe4d7c36c1708
Abdiox <124003288+Abdiox@users.noreply.github.com>
2025-12-10 18:32:08 +0100

    yes

---
+++ backend/src/main/java/org/pet/backendpetshelter/Entity/Vaccination.java
@@ -0,0 +1,37 @@
+
+package org.pet.backendpetshelter.Entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "vaccination")
+public class Vaccination {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "vaccination_id")
+    private Long id;
+
+
+    @ManyToOne
+    @JoinColumn(name = "animal_id")
+    private Animal animal;
+
+    @ManyToOne
+    @JoinColumn(name = "vet_id")
+    private Veterinarian veterinarian;
+
+    private Date dateAdministered;
+
+    @ManyToOne
+    @JoinColumn(name = "vaccination_type_id")
+    private VaccinationType vaccinationType;
+
+    private Date nextDueDate;
+}
