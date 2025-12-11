1e4144927277cf7d681ef0aeec446ccef6227bdd
Benjamin-Harris1 <beha1503@gmail.com>
2025-12-11 19:22:42 +0100

    Remove unused Spring Data Web Support configuration from application

--- backend/src/main/java/org/pet/backendpetshelter/BackendPetShelterApplication.java
+++ backend/src/main/java/org/pet/backendpetshelter/BackendPetShelterApplication.java
@@ -3,12 +3,8 @@ package org.pet.backendpetshelter;
 import io.sentry.Sentry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
-
-import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
-@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class BackendPetShelterApplication {
 
     public static void main(String[] args) {
